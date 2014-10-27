b53658508e145d2baff41bd2e6c550bcbfd32d21
binkula <jenn@thesecretlocation.com>
2014-10-27 09:55:08 -0600

    Update spiderApp.js

--- app/js/spiderApp.js
+++ app/js/spiderApp.js
@@ -1,24 +1,5 @@
 (function(){
     var app = angular.module('spiderLog', []);
-  
-    app.controller("SpiderController", ['$http', function($http){
-        this.name = "";
-        this.size = "";
-        this.got = "";
-        this.lastFed = "";
-        this.lastMolt = "";
-
-        var spider = this;
-
-        $http.get("spiders.json").success(function(data){
-            console.log(data);
-            spider.name = data.name;
-            spider.size = data.size;
-            spider.got = data.got;
-            spider.lastFed = data.lastFed;
-            spider.lastMolt = data.lastMolt;
-        });
-    } ]);
 
     app.directive("spiderList", function(){
         return{
